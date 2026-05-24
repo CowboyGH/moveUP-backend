05201c86332b07140415daf40b92c9b33774d696
Ryan Delaney <145113692+CowboyGH@users.noreply.github.com>
2026-05-24 17:13:39 +0700

    fix(schema): change duration_minutes from string to integer (#8)

--- database/migrations/0001_01_01_000004_create_tests_tables.php
+++ database/migrations/0001_01_01_000004_create_tests_tables.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
-            $table->string('duration_minutes');
+            $table->unsignedInteger('duration_minutes');
             $table->string('image')->nullable();
             $table->boolean('is_active');
             $table->timestamps();
