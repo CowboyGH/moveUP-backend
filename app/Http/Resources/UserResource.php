02739ee71f6fa922778026f6d7da0c11b21f0c11
CowboyGH <kocergindenis81@gmail.com>
2026-05-27 15:40:57 +0700

    fix(auth): include updated_at in UserResource response

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -19,6 +19,7 @@ class UserResource extends JsonResource
             'email_verified' => !is_null($this->email_verified_at),
             'email_verified_at' => $this->email_verified_at,
             'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
         ];
     }
 }
